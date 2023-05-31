f15b9f76d09e48eac8773379c736116bf1a0a4f8
GitAcrone <acrown@outlook.fr>
2023-06-01 00:29:26 +0200

    Amélioration commande info

--- cogs/core/core.py
+++ cogs/core/core.py
@@ -1,14 +1,18 @@
-from discord.ext import commands, tasks
-from discord import app_commands
-import discord
-import textwrap
-import platform
 import io
-import traceback
 import logging
+import platform
+import textwrap
+import traceback
 from contextlib import redirect_stdout
-from typing import Optional, Any
-from gpiozero import CPUTemperature, LoadAverage, DiskUsage
+from pathlib import Path
+from typing import Any, Optional
+
+import discord
+from discord import app_commands
+from discord.ext import commands, tasks
+from gpiozero import CPUTemperature, DiskUsage, LoadAverage
+
+from common import dataio
 
 logger = logging.getLogger(f'Wanderlust.{__name__.capitalize()}')
 
@@ -157,6 +161,43 @@ class Core(commands.Cog):
         embed.add_field(name="Température (CPU)", value=f"{cpu.temperature:.2f}°C")
         embed.add_field(name="Charge moyenne (CPU)", value=f"{load.load_average:.2f}%")
         embed.add_field(name="Espace disque utilisé", value=f"{disk.usage:.2f}%")
+        await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(name='info')
+    async def _get_bot_info(self, interaction: discord.Interaction):
+        """Obtenir des informations sur le bot"""
+        if not isinstance(self.bot.user, discord.User):
+            return await interaction.response.send_message("Impossible d'obtenir des informations sur le bot.", ephemeral=True)
+        
+        temp_colors = {
+            30: discord.Color.green(),
+            40: discord.Color.gold(),
+            50: discord.Color.orange(),
+            60: discord.Color.red()
+        }
+        
+        embed = discord.Embed(title=f"**Informations concernant `{self.bot.user}`**")
+        embed.description = f"***{self.bot.user.name}*** est un bot Discord développé par *{self.bot.get_user(int(self.bot.config['OWNER']))}* disponible depuis 4 Mai 2023."
+        
+        cpu = CPUTemperature()
+        load = LoadAverage()
+        disk = DiskUsage()
+        col = [v for k, v in temp_colors.items() if cpu.temperature < k][0]
+        inforasp = f"**Modèle** : `Raspberry Pi 4 Model B`\n**Temp. CPU** : `{cpu.temperature:.2f}°C`\n**Charge moy. CPU** : `{load.load_average:.2f}%`\n**Espace disque** : `{disk.usage:.2f}%`"
+        embed.add_field(name="Hébergement", value=inforasp)
+
+        sysinfo = f"**OS** : `{platform.system()} {platform.release()}`\n**Python** : `{platform.python_version()}`\n**discord.py** : `{discord.__version__}`\n**SQLite** : `{dataio.sqlite3.sqlite_version}`"
+        embed.add_field(name="Système", value=sysinfo)
+        
+        # Calcul de la place occupée par les données du bot
+        total_size = 0
+        for path in Path("data").rglob("*"):
+            total_size += path.stat().st_size
+        total_size = total_size / 1024 / 1024
+        embed.add_field(name="Données", value=f"**Taille** : `{total_size:.2f} Mo`\n**Nb. fichiers** : `{len(list(Path('data').rglob('*')))} fichiers`")
+        
+        embed.set_thumbnail(url=self.bot.user.display_avatar.url)
+        embed.color = col
         await interaction.response.send_message(embed=embed)
 
 async def setup(bot):
