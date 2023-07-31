d35d4967e2e0ee0a9b275d8a2e6ab3bab0de80b0
GitAcrone <acrown@outlook.fr>
2023-07-31 19:04:24 +0200

    Amélioration cache

--- cogs/chatter/chatter.py
+++ cogs/chatter/chatter.py
@@ -219,7 +219,8 @@ class CustomChatbot:
         
     def _get_embed(self):
         """Récupère un embed représentant le chatbot."""
-        em = discord.Embed(title=f'***{str(self)}***', description=f'*{self.description}*', color=self._get_avatar_color() if not self.id in self._cog.chatbots_colors else discord.Color(self._cog.chatbots_colors[self.id]))
+        cb_color = self._cog._get_chatbot_color(self)
+        em = discord.Embed(title=f'***{str(self)}***', description=f'*{self.description}*', color=cb_color)
         em.set_thumbnail(url=self.avatar_url)
         em.add_field(name="Prompt d'initialisation", value=f'```{pretty.troncate_text(self.system_prompt, 1000)}```', inline=False)
         em.add_field(name="Température", value=f'`{self.temperature}`')
@@ -764,13 +765,22 @@ class Chatter(commands.Cog):
             return ''
         return self.bot.user.display_avatar.url
     
+    def _cache_chatbot_color(self, chatbot: CustomChatbot) -> None:
+        """Met à jour la couleur d'un chatbot."""
+        self.chatbots_colors[chatbot] = chatbot._get_avatar_color()
+        
+    def _get_chatbot_color(self, chatbot: CustomChatbot) -> discord.Color:
+        """Récupère la couleur d'un chatbot."""
+        return self.chatbots_colors[chatbot] if chatbot in self.chatbots_colors else chatbot._get_avatar_color()
+    
     # Chatbots customs -----------------------------------------------------------------------------------------------
     
     def get_chatbot(self, guild: discord.Guild, profile_id: int, *, resume: bool = True, debug: bool = False) -> CustomChatbot:
         """Récupère un profil d'IA."""
         # On met à jour la couleur du chatbot
         chatbot = CustomChatbot(self, guild, profile_id, resume=resume, debug=debug)
-        self.chatbots_colors[chatbot] = chatbot._get_avatar_color()
+        if not chatbot in self.chatbots_colors:
+            self.chatbots_colors[chatbot] = chatbot._get_avatar_color()
         return chatbot
     
     def get_chatbot_by_name(self, guild: discord.Guild, name: str) -> CustomChatbot:
